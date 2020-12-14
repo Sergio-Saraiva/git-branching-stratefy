57c894730280515f270a5d665b5d2d8c37374716
Sérgio Saraiva <sergio.s.neto@outlook.com>
2020-12-14 15:16:08 -0300

    Test for ci 2

--- src/routes.js
+++ src/routes.js
@@ -4,4 +4,6 @@ const routes = new Router();
 
 routes.get('/', (req, res) => res.send('Hello World para o teste de CI'));
 
+routes.get('/rota2', (req, res) => res.send('Hello World para o teste de CI'));
+
 export default routes;
