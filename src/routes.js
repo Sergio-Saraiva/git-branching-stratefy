c0991fa7851ddb10ab5ea6923c3a7eed36cfb71b
Sérgio Saraiva <sergio.s.neto@outlook.com>
2020-12-14 15:19:58 -0300

    removed git tags

--- src/routes.js
+++ src/routes.js
@@ -4,10 +4,7 @@ const routes = new Router();
 
 routes.get('/', (req, res) => res.send('Hello World para o teste de CI'));
 
-<<<<<<< HEAD
 routes.get('/rota1', (req, res) => res.send('Hello World para o teste de CI'));
-=======
 routes.get('/rota2', (req, res) => res.send('Hello World para o teste de CI'));
->>>>>>> feature-testeCI2
 
 export default routes;
