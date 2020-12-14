cd182b70640ff3956f97489a2c3308dca3c13c40
Sérgio Saraiva de Sousa Neto <sergio.s.neto@alu.ufc.br>
2020-12-14 17:22:45 -0300

    O breno
    é brabo

--- src/routes.js
+++ src/routes.js
@@ -8,4 +8,6 @@ routes.get('/rota1', (req, res) => res.send('Hello World para o teste de CI'));
 routes.get('/rota2', (req, res) => res.send('Hello World para o teste de CI'));
 routes.get('/rotateste', (req, res) => res.send('Rota teste git'));
 
+// mudança
+
 export default routes;
